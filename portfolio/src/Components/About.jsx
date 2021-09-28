b451cf8cc30d0bb0d55041cdb8525b3a7517a1f0
Vaibhav Prakash <58112154+vbhvprksh@users.noreply.github.com>
2021-09-28 23:41:19 +0530

    skills done

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -35,35 +35,124 @@ function About() {
                                     I believe that I should make myself better in all of these aspects.</div>     
                                 <div className="contthree">
                                     <div className="one">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Birthday:</strong>19th October 1999.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Birthday: 19th October 1999</strong>
                                     <div className="one-one">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age:</strong>30.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age: 22</strong>
                                     </div>
                                     </div>
                                     <div className="one">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Email:</strong>vbhvprksh@gmail.com
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Email:  vbhvprksh@gmail.com</strong>
                                     <div className="one-two">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age:</strong>30.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Degree: B.Tech</strong>
                                     </div>
                                     </div>
                                     <div className="one">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Contact No:</strong>6201143373.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Contact No: 6201143373</strong>
                                     <div className="one-three">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age:</strong>30.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age: 30</strong>
                                     </div>
                                     </div>
                                     <div className="one">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Degree:</strong>B.Tech.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Degree: B.Tech</strong>
                                     <div className="one-four">
-                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age:</strong>30.
+                                    <span className="icon"><FaAngleRight></FaAngleRight></span><strong>Age:30</strong>
                                     </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+                    </div>
+                <div className="intro">
+                    SKILLS <hr></hr>
+                </div>
+                <div className="skills-bar">
+                    <div className="skill-bar1">
+                    <div className="bar">
+                        <div className="info">
+                            <span>HTML</span>
+                        </div>
+                        <div className="progress-line html">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>CSS</span>
+                        </div>
+                        <div className="progress-line css">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>JS</span>
+                        </div>
+                        <div className="progress-line js">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>Bootstrap</span>
+                        </div>
+                        <div className="progress-line bootstrap">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>React</span>
+                        </div>
+                        <div className="progress-line react">
+                            <span></span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="skill-bar2">
+                    <div className="bar">
+                        <div className="info">
+                            <span>HTML</span>
+                        </div>
+                        <div className="progress-line html">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>CSS</span>
+                        </div>
+                        <div className="progress-line css">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>JS</span>
+                        </div>
+                        <div className="progress-line js">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>Bootstrap</span>
+                        </div>
+                        <div className="progress-line bootstrap">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>React</span>
+                        </div>
+                        <div className="progress-line react">
+                            <span></span>
+                        </div>
+                    </div>
+                
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
